c03830f8ffa2287266a8eb3fb6441cbcfb4c92e7
Omar Burney <omar-macbook@Omars-MacBook-Pro.local>
2024-07-15 21:34:57 -0700

    upgrade react to v18

--- lib/ChatInput/index.d.ts
+++ lib/ChatInput/index.d.ts
@@ -1,7 +1,7 @@
-/// <reference types="react" />
+import React from 'react';
 interface ChatInputProps {
     inputStyles?: object;
     inputPlaceholder?: string;
 }
-declare const ChatInput: (props: ChatInputProps) => JSX.Element;
+declare const ChatInput: (props: ChatInputProps) => React.JSX.Element;
 export default ChatInput;
